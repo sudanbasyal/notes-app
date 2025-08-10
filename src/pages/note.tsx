28364de22be1ce28743e0392d539967347bf11b1
Sudan Basyal <sudanbasyal74@gmail.com>
2025-08-10 22:43:56 +0545

    fix: layout fixes that include padding

--- src/pages/note.tsx
+++ src/pages/note.tsx
@@ -7,12 +7,12 @@ export default function NotesDashboard() {
   return (
     <section className="flex flex-col h-full rounded-lg border border-gray-200 ">
       <div className="flex flex-col  rounded-lg ">
-        <div className="w-full flex justify-between border-b py-2 px-4">
+        <div className="w-full flex justify-between border-b py-1.5 px-4">
           <h3 className="text-title1 font-semibold text-primary">EasyNotes</h3>
           <UserDropdown />
         </div>
         <Header />
-        <div className="grid grid-cols-1 pt-2 pb-4 px-4 md:px-0 md:grid-cols-[230px_1fr] gap-8">
+        <div className="grid grid-cols-1 p-4 md:px-0 md:grid-cols-[230px_1fr] gap-8">
           <div className="hidden md:flex ">
             <Sidebar />
           </div>
